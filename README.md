41145842a15f377c53ea9d197ac59972ba97036c
YoNG-Zaii <yongzaii811@outlook.com>
2023-02-25 17:47:45 +0800

    feat: Added Linear access

--- README.md
+++ README.md
@@ -16,11 +16,14 @@ jobs:
   linear-issue-extaction:
     runs-on: ubuntu-latest
     steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+        
       - name: Extract
         id: extract
-        uses: YoNG-Zaii/linear-actions@v1.0
+        uses: swe-memory/linear-actions@v1.0
         with:
-          branch_name: ${{ github.ref_name }}
+          branch_name: ${{ github.head_ref }}
 
       - name: Output
         run: echo "${{ steps.extract.outputs.issue_id }}"
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const linear = require('@linear/sdk')
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
   const branchName = core.getInput('branch_name');
+  const apiKey = core.getInput('linear_api_key');
   console.log(`Current branch: ${branchName}`);
+
   const regex = /[Ss][Ww][Ee]-[1-9][0-9]*/;
   const found = branchName.match(regex);
   console.log(found);
   core.setOutput("issue_id", found[0]);
+
+  if(found){
+    const linearClient = new LinearClient({
+      apiKey: apiKey
+    })
+
+    const issues = await linearClient.issues();
+    console.log(issues);
+  }
+  
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
